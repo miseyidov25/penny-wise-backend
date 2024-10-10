40be81d8c5ef4adff1c22796333b709b278646c0
MirIsmayil Seyidov <43446910+miseyidov25@users.noreply.github.com>
2024-10-10 10:05:20 +0300

    Added wallet system,  small bug fix (#12)
    * chore: auth fix
    
    * feat: add wallets
    
    * chore: removed unneeded tests

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +7,3 @@ Route::get('/', function () {
 });
 
 require __DIR__.'/auth.php';
-
-Route::resource('categories', CategoryController::class)->middleware('auth');
-Route::resource('transactions', TransactionController::class)->middleware('auth');
