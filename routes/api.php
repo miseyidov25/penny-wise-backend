66c83e8aefab5e477b6b144fae4191fd2e204e7e
MirIsmayil <miseyidov25@gmail.com>
2024-10-10 09:53:52 +0300

    feat: add wallets

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WalletController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TransactionController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('wallets', WalletController::class)->middleware('auth');
+Route::resource('categories', CategoryController::class)->middleware('auth');
+Route::resource('transactions', TransactionController::class)->middleware('auth');
